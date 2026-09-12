cc5b6e4810091f74af444d65cd1aff2778955abd
rashiyamani <rashiyamani2@gmail.conm>
2026-09-12 23:29:06 +0530

    Fix undefined variable in DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,6 +15,6 @@ public class DemoApplication {
 
     @GetMapping("/")
     public String home() {
-        return "Hello from Java App running on Amazon EKS!";
+        return "Hello from Java App running on Amazon EKS! 11:35";
     }
 }
