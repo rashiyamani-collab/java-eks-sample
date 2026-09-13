8b3f5d239941bf77e78bf6eb88b7471ca0f7db26
rashiyamani-collab <97337399+rashiyamani-collab@users.noreply.github.com>
2026-09-13 13:50:23 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,6 +15,6 @@ public class DemoApplication {
 
     @GetMapping("/")
     public String home() {
-        return "Hello from Java App running on Amazon EKS! 11:40";
+        return "Hello from Java App running on Amazon EKS! 1:50 PM";
     }
 }
