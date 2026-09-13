45ce6c54a95e7e56a89a2f19e242954e20da3876
rashiyamani <rashiyamani2@gmail.conm>
2026-09-13 17:55:27 +0530

    sonar file added

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,6 +15,6 @@ public class DemoApplication {
 
     @GetMapping("/")
     public String home() {
-        return "Hello from Java App running on Amazon EKS! 4:10 PM";
+        return "Hello from Java App running on Amazon EKS! 5:50 PM";
     }
 }
